6171deb074888c4e886511ee45394c1506d0ab0f
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-04 23:28:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I BUILT THIS\nOr did I??"
+    "Started my server using Shotgun!"
   end
 
 end
